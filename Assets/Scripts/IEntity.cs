0d0e6bf38b2127fc1b28fa0757eb9f4a08275c61
振炫 邱 <2296282722@qq.com>
2023-03-12 16:12:33 +0800

    init

---
+++ Assets/Scripts/IEntity.cs
@@ -0,0 +1,13 @@
+﻿using Assets.Classes;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+	public interface IEntity
+	{
+		public string Name { get; set; }
+		public IEnumerable<IAbility> Abilities { get; }
+	}
+}
