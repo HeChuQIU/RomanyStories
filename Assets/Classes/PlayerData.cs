4dfdf8cadde3d4de8a3ab1f7efba2f6a33774149
振炫 邱 <2296282722@qq.com>
2023-03-31 21:20:10 +0800

    同步

--- Assets/Classes/PlayerData.cs
+++ Assets/Classes/PlayerData.cs
@@ -17,6 +17,10 @@ namespace Assets.Classes
 
 	public struct PlayerData : IPlayerData
 	{
-
+		public int MaxHealth { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+		public int CurrentHealth { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+		public int MaxStrength { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+		public int Strength { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+		public int MaxArmor { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 	}
 }
